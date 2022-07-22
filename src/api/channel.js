4b45e98065d882d0e01cde202ede1da065446f99
wcy980710 <744930977@qq.com>
2022-07-22 23:26:25 +0800

    第4天完成

--- src/api/channel.js
+++ src/api/channel.js
@@ -7,3 +7,20 @@ export const getAllChannels = () => {
     url: '/v1_0/channels'
   })
 }
+
+export function addUserChannel (channel) {
+  return request({
+    method: 'PATCH',
+    url: '/v1_0/user/channels',
+    data: {
+      channels: [channel]
+    }
+  })
+}
+
+export function deleteUserChannel (channelId) {
+  return request({
+    method: 'DELETE',
+    url: `/v1_0/user/channels/${channelId}`
+  })
+}
--- src/views/shouye/components/channel-edit.vue
+++ src/views/shouye/components/channel-edit.vue
@@ -1,56 +1,65 @@
 <template>
   <div class="channel-edit">
-    <!-- 我的频道 -->
     <van-cell :border="false">
-      <template #title>
-        <div class="title-text">我的频道</div>
-      </template>
-      <van-button type="danger" plain round size="mini" class="edit-btn"
-        >编辑</van-button
+      <div slot="title" class="title-text">我的频道</div>
+      <van-button
+        class="edit-btn"
+        size="mini"
+        round
+        type="danger"
+        plain
+        @click="isEdit = !isEdit"
       >
+        {{ isEdit ? "完成" : "编辑" }}
+      </van-button>
     </van-cell>
-    <van-grid class="my-grid" :gutter="10">
+    <van-grid :gutter="10" class="my-grid">
       <van-grid-item
         class="grid-item"
-        v-for="(channel, index) in MyChannels"
-        :key="index"
-        icon="clear"
+        v-for="(channel, index) in myChannels"
+        :key="channel.id"
+        @click="onMyChannelClick(channel, index)"
       >
-        <template #text>
-          <span :class="{ active: index === active, text: true }">
-            {{ channel.name }}
-          </span>
-        </template>
+        <van-icon
+          slot="icon"
+          name="clear"
+          v-show="isEdit && !fixedChannels.includes(channel.id)"
+        ></van-icon>
+        <span :class="{ active: index === active, text: true }" slot="text">
+          {{ channel.name }}
+        </span>
       </van-grid-item>
     </van-grid>
-    <!-- /我的频道 -->
     <!-- 频道推荐 -->
     <van-cell :border="false">
-      <template #title>
-        <div class="title-text">频道推荐</div>
-      </template>
+      <div slot="title" class="title-text">频道推荐</div>
     </van-cell>
-    <van-grid class="recommend-grid" :gutter="10">
+    <van-grid :gutter="10" class="recommend-grid">
       <van-grid-item
         class="grid-item"
         v-for="(channel, index) in recommendChannels"
         :key="index"
         icon="plus"
         :text="channel.name"
+        @click="onAddChannel(channel)"
       />
     </van-grid>
     <!-- /频道推荐 -->
   </div>
 </template>
-
 <script>
-import { getAllChannels } from '@/api/channel'
+import { mapState } from 'vuex'
+import {
+  getAllChannels,
+  addUserChannel,
+  deleteUserChannel
+} from '@/api/channel'
+import { setItem, getItem } from '@/utils/storage'
+
 export default {
-  // 组件名称
   name: 'ChannelEdit',
-  // 组件参数 接收来自父组件的数据
   props: {
-    MyChannels: {
+    myChannels: {
       type: Array,
       required: true
     },
@@ -59,71 +68,100 @@ export default {
       required: true
     }
   },
-  // 局部注册的组件
-  components: {},
-  // 组件状态值
   data () {
     return {
-      allChannels: [] // 所有频道
+      allChannels: [],
+      isEdit: false,
+      fixedChannels: [0]
     }
   },
-  // 计算属性
   computed: {
     recommendChannels () {
-      const channels = []
-      this.allChannels.forEach((channel) => {
-        // 遍历所有频道，如果不在我的频道中，就放入空数组channels
-        // find 遍历数组，找到满足条件的元素项
-        const ret = this.MyChannels.find((MyChannels) => {
-          // 在我的频道中查找是否有此频道
-          return MyChannels.id === channel.id
-        })
-        // 如果不存在，放入空数组
-        if (!ret) {
-          channels.push(channel)
-        }
-      })
-
-      // 把计算结果返回
-      return channels
-    }
+      return this.allChannels.filter(
+        (it) => !this.myChannels.find((channel) => +channel.id === +it.id)
+      )
+    },
+    ...mapState(['user'])
   },
-  // 侦听器
-  watch: {},
-  // 组件方法
-  methods: {
-    async loadAllChannels () {
-      try {
-        const { data } = await getAllChannels()
-        this.allChannels = data.data.channels
-      } catch (err) {
-        this.$toast('获取频道列表失败')
-      }
-    }
-  },
-  // 以下是生命周期钩子   注：没用到的钩子请自行删除
-  /**
-   * 组件实例创建完成，属性已绑定，但DOM还未生成，$ el属性还不存在
-   */
   created () {
     this.loadAllChannels()
   },
-  /**
-   * el 被新创建的 vm.$ el 替换，并挂载到实例上去之后调用该钩子。
-   * 如果 root 实例挂载了一个文档内元素，当 mounted 被调用时 vm.$ el 也在文档内。
-   */
-  mounted () {}
+  methods: {
+    async loadAllChannels () {
+      // 1. 如果未登录 && 有本地数据：使用本地数据
+      const channels = getItem('TOUTIAO_CHANNELS')
+      if (!this.user && channels) {
+        this.allChannels = channels
+      } else {
+        // 2. 获取远程数据
+        try {
+          const { data } = await getAllChannels()
+          this.allChannels = data.data.channels
+        } catch (err) {}
+      }
+    },
+    async onAddChannel (channel) {
+      /* eslint-disable-next-line */
+      this.myChannels.push(channel);
+      if (this.user) {
+        // 存储远程服务器
+        try {
+          await addUserChannel({
+            id: channel.id,
+            seq: this.myChannels.length
+          })
+          this.$toast('添加成功')
+        } catch (err) {
+          this.$toast('添加失败')
+        }
+      } else {
+        // 存储本地
+        setItem('TOUTIAO_CHANNELS', this.myChannels)
+      }
+    },
+    onMyChannelClick (channel, index) {
+      if (this.isEdit) {
+        if (this.fixedChannels.includes(channel.id)) {
+          return
+        }
+
+        /* eslint-disable-next-line */
+        this.myChannels.splice(index, 1);
+
+        if (index <= this.active) {
+          this.$emit('update-active', index - 1)
+        }
+
+        // 持久化数据
+        this.deleteChannel(channel)
+      } else {
+        this.$emit('update-active', index, false)
+      }
+    },
+    // 删除时持久化存储频道数据
+    async deleteChannel (channel) {
+      if (!this.user) {
+        setItem('TOUTIAO_CHANNELS', this.myChannels)
+      } else {
+        try {
+          await deleteUserChannel(channel.id)
+          this.$toast('删除成功')
+        } catch (err) {
+          this.$toast('删除失败')
+        }
+      }
+    }
+  }
 }
 </script>
-
 <style scoped lang="less">
 .channel-edit {
   padding: 85px 0;
-
   .title-text {
     font-size: 32px;
-    color: #333;
+    color: #333333;
   }
+
   .edit-btn {
     width: 104px;
     height: 48px;
@@ -131,45 +169,52 @@ export default {
     color: #f85959;
     border: 1px solid #f85959;
   }
-  /deep/ .my-grid {
-    .grid-item {
-      .van-icon-clear {
-        position: absolute;
-        right: -10px;
-        top: -10px;
-        font-size: 30px;
-        color: #cacaca;
-        z-index: 20;
-      }
+}
+/deep/ .grid-item {
+  width: 160px;
+  height: 86px;
+  .van-grid-item__content {
+    white-space: nowrap;
+    background-color: #f4f5f6;
+    .van-grid-item__icon-wrapper {
+      position: unset;
+    }
+    .van-grid-item__text,
+    .text {
+      font-size: 28px;
+      color: #222;
+      margin-top: 0;
+    }
+    .active {
+      color: red;
     }
   }
-  /deep/ .grid-item {
-    width: 160px;
-    height: 86px;
+}
+
+/deep/ .recommend-grid {
+  .grid-item {
     .van-grid-item__content {
-      white-space: nowrap;
-      background-color: #f4f5f6;
-      .van-grid-item__text,
-      .text {
+      flex-direction: row;
+      .van-icon-plus {
         font-size: 28px;
-        color: #222;
+        margin-right: 6px;
+      }
+      .van-grid-item__text {
         margin-top: 0;
       }
-      .active {
-        color: red;
-      }
     }
   }
+}
 
-  /deep/ .recommend-grid {
-    .grid-item {
-      .van-grid-item__content {
-        flex-direction: row;
-        .van-icon-plus {
-          font-size: 28px;
-          margin-right: 6px;
-        }
-      }
+/deep/ .my-grid {
+  .grid-item {
+    .van-icon-clear {
+      position: absolute;
+      right: -10px;
+      top: -10px;
+      font-size: 30px;
+      color: #cacaca;
+      z-index: 2;
     }
   }
 }
