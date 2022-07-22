bd9e69abcab64a265385afd640b90088f44f6533
wcy980710 <744930977@qq.com>
2022-07-22 11:58:38 +0800

    提交

--- src/components/article-item/index.vue
+++ src/components/article-item/index.vue
@@ -1,10 +1,16 @@
 
 <template>
   <van-cell class="article-item">
-    <div slot="title" class="title van-multi-ellipsis--l2">{{ article.title }}</div>
+    <div slot="title" class="title van-multi-ellipsis--l2">
+      {{ article.title }}
+    </div>
     <div slot="label">
       <div v-if="article.cover.type === 3" class="cover-wrap">
-        <div class="cover-item" v-for="(img, index) in article.cover.images" :key="index">
+        <div
+          class="cover-item"
+          v-for="(img, index) in article.cover.images"
+          :key="index"
+        >
           <van-image class="cover-item-img" fit="cover" :src="img" />
         </div>
       </div>
@@ -46,44 +52,44 @@ export default {
 </script>
 
 <style scoped lang="less">
-  .article-item {
-    .title {
-      font-size: 32px;
-      color: #3a3a3a;
-    }
+.article-item {
+  .title {
+    font-size: 32px;
+    color: #3a3a3a;
+  }
 
-    .van-cell__value {
-      flex: unset;
-      width: 232px;
+  .van-cell__value {
+    flex: unset;
+    width: 232px;
+    height: 146px;
+    padding-left: 25px;
+  }
+
+  .right-cover {
+    width: 232px;
+    height: 146px;
+  }
+
+  .label-info-wrap span {
+    font-size: 22px;
+    color: #b4b4b4;
+    margin-right: 25px;
+  }
+
+  .cover-wrap {
+    display: flex;
+    padding: 30px 0;
+    .cover-item {
+      flex: 1;
       height: 146px;
-      padding-left: 25px;
-    }
-
-    .right-cover {
-      width: 232px;
-      height: 146px;
-    }
-
-    .label-info-wrap span {
-      font-size: 22px;
-      color: #b4b4b4;
-      margin-right: 25px;
-    }
-
-    .cover-wrap {
-      display: flex;
-      padding: 30px 0;
-      .cover-item {
-        flex: 1;
+      &:not(:last-child) {
+        padding-right: 4px;
+      }
+      .cover-item-img {
+        width: 100%;
         height: 146px;
-        &:not(:last-child) {
-          padding-right: 4px;
-        }
-        .cover-item-img {
-          width: 100%;
-          height: 146px;
-        }
       }
     }
   }
+}
 </style>
--- src/views/shouye/index.vue
+++ src/views/shouye/index.vue
@@ -74,76 +74,88 @@ export default {
 
 <style scoped lang="less">
 .shouye-container {
-  padding-top: 93px;
+  padding-top: 174px;
   padding-bottom: 100px;
-  /deep/.van-nav-bar__title {
+  .van-nav-bar__title {
     max-width: unset;
-     .search-btn {
-    width: 555px;
-    height: 63px;
-    background-color: #5babfb;
-    border: none;
-    font-size: 28px;
-    .van-icon {
-      font-size: 32px;
-    }
   }
-   .channel-tabs {
-    .van-tabs__wrap {
-      position: fixed;
-      top: 92px;
-      z-index: 2;
-      width: 100%;
-      height: 82px;
-    }
-    .van-tab {
-      border-right: 1px solid #edeff3;
-      min-width: 200px;
-      height: 80px;
-      .van-tab__text {
-        font-size: 27px;
-        color: #777777;
-      }
-    }
+}
+/deep/ .van-nav-bar__title {
+  max-width: unset;
+}
+.search-btn {
+  width: 555px;
+  height: 64px;
+  background-color: #5babfb;
+  border: none;
+  font-size: 28px;
+  .van-icon {
+    font-size: 32px;
+  }
+}
 
-    .van-tab--active .van-tab__text {
-      color: #333 !important;
-    }
-    .van-tabs__line {
-      width: 31px !important;
-      height: 6px;
-      background: rgba(50, 150, 250, 1);
-      border-radius: 3px;
-      bottom: 8px;
-    }
-    .van-tabs__nav {
-      padding: 0;
-    }
-  }
-  .hamburger-btn {
+/deep/ .channel-tabs {
+  .van-tabs__wrap {
     position: fixed;
+    top: 92px;
+    z-index: 1;
+    left: 0;
     right: 0;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 66px;
     height: 82px;
-    background-color: #fff;
-    opacity: 0.902;
-    i.toutiao {
-      font-size: 33px;
-    }
-    &:before {
-      content: "";
-      position: absolute;
-      left: 0;
-      width: 1px;
-      height: 100%;
-      background-image: url(~@/assets/gradient-gray-line.png);
-      background-size: contain;
-    }
-  }
   }
 
+  .van-tab {
+    border-right: 1px solid #edeff3;
+    min-width: 200px;
+    font-size: 30px;
+    color: #777777;
+  }
+
+  .van-tab--active {
+    color: #333333;
+  }
+
+  .van-tabs__nav {
+    padding-bottom: 0;
+  }
+
+  .van-tabs__line {
+    bottom: 8px;
+    width: 31px !important;
+    height: 6px;
+    background-color: #3296fa;
+  }
+}
+
+.placeholder {
+  //占位元素为了解决最后一个tab标签被汉堡盖住问题（看下边的图）
+  flex-shrink: 0; //此元素不参与flex的
+  width: 66px;
+  height: 82px;
+}
+
+.hamburger-btn {
+  position: fixed;
+  right: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 66px;
+  height: 82px;
+  background-color: #fff;
+  opacity: 0.902;
+  i.toutiao {
+    font-size: 33px;
+  }
+  &:before {
+    //找到父节点，添加伪元素（这个是汉堡按钮与tab之间的一个竖线）
+    content: "";
+    position: absolute;
+    left: 0;
+    width: 1px; //在模拟器上看不到（手机上没问题），如果想看到改为2px
+    height: 100%;
+    background-image: url(~@/assets/gradient-gray-line.png);
+    background-size: contain;
+  }
 }
 </style>
